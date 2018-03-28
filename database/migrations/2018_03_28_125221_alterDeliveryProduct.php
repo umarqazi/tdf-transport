430a00ad3eeceadbffb2aee4f264dab7d537b116
adimalik <muhammad.adeel@gems.techverx.com>
2018-03-28 19:00:14 +0500

    added migration files

---
+++ database/migrations/2018_03_28_125221_alterDeliveryProduct.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterDeliveryProduct extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+     public function up()
+     {
+         Schema::table('deliveries', function($t) {
+           $t->integer('product_id')->unsigned()->nullable();
+           $t->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+         });
+     }
+
+
+     public function down()
+     {
+         Schema::table('deliveries', function($t) {
+             $t->dropColumn('product_id');
+         });
+     }
+}
