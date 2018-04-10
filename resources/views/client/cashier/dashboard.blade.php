5351b9b5a0e9b6bb26a6782620f4e3a4eb312b2f
adimalik <muhammad.adeel@gems.techverx.com>
2018-04-10 18:25:36 +0500

    added class in checkbox

--- resources/views/client/cashier/dashboard.blade.php
+++ resources/views/client/cashier/dashboard.blade.php
@@ -160,7 +160,7 @@ TDF Dashboard
                 <td><a href="{{URL::to('/delivery', ['id'=>$dayDelivery['id']])}}" ><i class="fa fa-edit fa-fw"></i></a></td>
                 <td><a href="{{URL::to('/deleteDelivery', ['id'=>$dayDelivery['id']])}}" class="delete"><i class="fa fa-trash-o fa-fw"></i></a></td>
                 @if($authUser->type==Config::get('constants.Users.Manager'))
-                <td><input type="checkbox" name="delivery_id[]" value="{{$dayDelivery['id']}}" {{($dayDelivery['status']=='1')?'checked disabled':''}} class="" @if($key!=date('d-M-Y', strtotime($nextDate))) disabled @endif>
+                <td><input type="checkbox" class="deliveryCheckbox" name="delivery_id[]" value="{{$dayDelivery['id']}}" {{($dayDelivery['status']=='1')?'checked disabled':''}} class="" @if($key!=date('d-M-Y', strtotime($nextDate))) disabled @endif>
                 </td>
                 @endif
               </tr>
