b139462403812dbf4a95f72e49da6104f312036d
adimalik <muhammad.adeel@gems.techverx.com>
2018-04-12 14:17:32 +0500

    changing in driver tour is done

--- resources/views/client/email/driver_tours.blade.php
+++ resources/views/client/email/driver_tours.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Tour Plan</title>
+    <title>Planning des livraisons</title>
     {!! HTML::style('assets/css/bootstrap.css') !!}
     {!! HTML::style('assets/css/main.css') !!}
     {!! HTML::style('//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css') !!}
@@ -99,13 +99,9 @@
                               <ul class="list-unstyled icons driver-plans">
                             <li><strong>Client: </strong>{{$tour['delivery']['first_name']}} {{$tour['delivery']['last_name']}}</li>
                             <li><strong>Numéro et rue: </strong>{{$tour['delivery']['address']}} </li>
-                            <li><strong>Ville & Code Postal: </strong>{{$tour['delivery']['city']}} {{$tour['delivery']['postal_code']}}</li>
-                            <li><strong>Téléphone mobile: </strong> {{$tour['delivery']['mobile_number']}}</li>
-                            <li><strong>Téléphone fixe: </strong> {{$tour['delivery']['landline']}}</li>
+                            <li><strong>Code postal & Ville: </strong>{{$tour['delivery']['postal_code']}} {{$tour['delivery']['city']}}</li>
                             <li><strong>Produit commandé: </strong> {{($tour['delivery']['product_type']!='')? $tour['delivery']['product_type']: 'Multi-produits'}}</li>
-                            <li><strong>Prix de la livraison: </strong>{{($tour['delivery']['product_type']!='')? $tour['delivery']['delivery_charges']: 'Gratuit'}}</li>
                             <li><strong>Numéro de commande: </strong> {{$tour['delivery']['order_id']}}</li>
-                            @if($tour['delivery']['delivery_number'])<li><strong>Numéro du bon de livraison: </strong> {{$tour['delivery']['delivery_number']}}</li>@endif
                             <li><strong>Fonction de prestation: </strong> {{$tour['delivery']['service']}}</li>
                           </ul></a>
                       </td>
