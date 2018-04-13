0aecb154c8aeb996723e565431447301f67a667b
adimalik <muhammad.adeel@gems.techverx.com>
2018-04-13 10:41:06 +0500

    date field fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -222,6 +222,7 @@ class HomeController extends Controller
 	{
 		$name=$request->customer_name;
 		$order_id=$request->order_id;
+		$request->datetime= str_replace('/', '-', $request->datetime);
 		$date=Carbon::parse($request->datetime)->format('Y-m-d h:i:s');
 		$searchResult='';
 		$getDeliveryRecords=self::searchResults($request->all());
