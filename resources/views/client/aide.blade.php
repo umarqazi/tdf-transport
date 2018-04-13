af1bb4cbf61a2945db887c897d70f5d126272a53
adimalik <muhammad.adeel@gems.techverx.com>
2018-04-13 16:36:19 +0500

    change aide page style

--- resources/views/client/aide.blade.php
+++ resources/views/client/aide.blade.php
@@ -8,10 +8,10 @@
 <div class="besoin-block">
     <h2>Besoin d'aide ?</h2>
     <p>En cas de problème vous pouvez contacter la société TDF Transport, en cliquant sur le bouton ci-dessous.</p>
-    <a href="mailto:contact@tdf-transport.com" class="blue-btn">Cliquez</a>
+    <a href="mailto:contact@tdf-transport.com" class="blue-btn">Cliquez ici</a>
 </div>
 <div class="col-lg-12 text-center">
-  <a href="{{url('/dashboard')}}" class="back-button">Retour <i class="fa fa-arrow-circle-left"></i></a>
+  <a href="{{url('/dashboard')}}" class="back-button retour-btn">Retour <i class="fa fa-arrow-circle-left"></i></a>
 </div>
     <div class="aide-footer">
         © 2018 | Web Application de livraisons V1.0 | by annie
