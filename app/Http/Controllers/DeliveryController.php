61da4ddfb0742c73628ff24a7a1ebc1f24e30bf1
adimalik <muhammad.adeel@gems.techverx.com>
2018-04-17 15:17:43 +0500

    sms message updated

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -403,12 +403,10 @@ class DeliveryController extends Controller
       $date=Date::parse($request->date)->format('Y-m-d');
       $customerDetail=TourPlan::leftJoin('deliveries', 'tour_plan.delivery_id', '=', 'deliveries.id')->leftJoin('time_slot', 'tour_plan.time_slot_id', '=', 'time_slot.id')->leftJoin('stores', 'deliveries.store_id', '=', 'stores.id')->where('datetime', $date)->where('tour_plan.user_id', $request->id)->select('deliveries.datetime','deliveries.mobile_number','tour_plan.user_id','stores.store_name','time_slot.time','stores.phone_number')->get();
       foreach($customerDetail as $customer){
-        $message="Cher(e) client(e),
-
-        Votre commande sera livrée le ".$customer['datetime']." entre ".$customer['time'].", merci
-
-        ".$customer['store_name']."
-        ".$customer['phone_number'];
+        $message=
+"Cher(e) client(e), Votre commande sera livrée le ".Date::parse($customer['datetime'])->format('D/M/Y')." entre ".$customer['time'].", merci
+".$customer['store_name']."
+".$customer['phone_number'];
         $user=$customer['mobile_number'];
         $sendSMS=Ovh::checkSms($user, $message);
       }
